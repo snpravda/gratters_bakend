97a2b9c4f93ea8e542aa8e590bcb746187b4761c
snpravda <sn.pravda@gmail.com>
2020-12-17 14:23:50 +0200

    created and autenticated my account in telegram

--- gratters/views.py
+++ gratters/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
-from rest_framework import generics, status, parsers, renderers
+from rest_framework import generics, status, parsers, renderers, mixins
 from rest_framework.response import Response
 from .serializers import *
 from .filters import *
 from gratters_backend.settings import REST_FRAMEWORK
 # Create your views here.
 
-class GratterView(generics.ListCreateAPIView):
+class GratterView(generics.ListAPIView, mixins.CreateModelMixin):
     serializer_class = GratterSerializer
     queryset = Gratter.objects.all()
     filter_class  = GratterFilter
     parser_classes = ( parsers.FormParser, parsers.MultiPartParser, parsers.JSONParser)
     renderer_classes = (renderers.JSONRenderer,)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
