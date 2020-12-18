3c630ee34595b56354f69a5f62d1af37a46bf380
snpravda <sn.pravda@gmail.com>
2020-12-18 10:55:30 +0200

    some changes in url naming

--- gratters/urls.py
+++ gratters/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('gratter/', GrattersView.as_view()),
-    path('gratter/<int:pk>', SingleGratterView.as_view())
+    path('', GrattersView.as_view()),
+    path('<int:pk>/', SingleGratterView.as_view())
 ]
